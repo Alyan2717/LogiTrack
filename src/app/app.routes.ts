2be52172b8b1ab0901d4447ac5037337e3323c7a
Alyan2717 <outsource.developer1@decibel360cloud.com>
2025-06-03 17:46:46 +0200

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // {
+    //     path: 'login',
+    //     component: LoginComponent,
+    //     canActivate: [loginGuard]
+    // },
+    {
+        path: 'dashboard',
+        component: DashboardComponent,
+        //canActivate: [authGuard]
+    },
+    // {
+    //     path: 'contacts',
+    //     component: ContactComponent
+    // },
+    { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+    { path: '**', redirectTo: '/dashboard' }
+];
